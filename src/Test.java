064a217ca09d0cf6e43e822a7b033abab6672078
lisi <lisi@qq.com>
2019-01-02 15:55:29 +0800

    didi

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,6 @@ public class Test {
     //程序的入口
     public static void main(String[] args) {
         //打印输出ok
-        //打印输出ok
         System.out.println("okokok");
         System.out.println("21");
         System.out.println("okokok");
